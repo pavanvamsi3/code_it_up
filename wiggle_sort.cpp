d857624ddf3f369de92558a63c248e5837c0be97
Pavan Vamsi <sherlock@Pavans-MacBook-Pro.local>
2019-09-04 00:54:10 +0530

    Added wiggle sort solution

---
+++ wiggle_sort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void wiggle_sort(vector<int> &v)
+{
+	bool less = true;
+
+	for(int i=0; i<v.size()-1; i++) {
+		if (less) {
+			if(v[i] > v[i+1]) 
+				swap(v[i], v[i+1]);
+		} else {
+			if(v[i] < v[i+1])
+				swap(v[i], v[i+1]);
+		}
+		less = !less;
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int>v;
+	
+	for(int i=0; i<n; i++) {
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	
+	wiggle_sort(v);
+
+	for(int i=0; i<n; i++) {
+		cout << v[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
+
