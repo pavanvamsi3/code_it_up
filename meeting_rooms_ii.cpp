c8f23e5a5516b0b0f4fff3636b1b5118f37247a2
Pavan Vamsi <pavanvamsi3@gmail.com>
2019-09-04 23:52:48 +0530

    Added gitignore, also meeting_rooms_ii

---
+++ meeting_rooms_ii.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct interval {
+    int start;
+    int end;
+};
+
+bool sort_by_start(interval i, interval j)
+{
+    return i.start < j.start;
+}
+
+int get_meeting_rooms(vector<interval>&v)
+{
+    if (v.empty()) {
+        return 0;
+    }
+    sort(v.begin(), v.end(), sort_by_start);
+
+    int rooms = 1;
+    int min_end_time = v[0].end;
+
+    for(int i=1; i<v.size(); i++) {
+        if (v[i].start < min_end_time) {
+            rooms++;
+        }
+        if (v[i].end < min_end_time) {
+            min_end_time = v[i].end;
+        }
+    }
+    return rooms;
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<interval>v;
+
+    for(int i=0; i<n; i++) {
+        int start;
+        int end;
+
+        interval in;
+
+        cin >> start;
+        cin >> end;
+        in.start = start;
+        in.end = end;
+
+        v.push_back(in);
+    }
+    
+    cout << get_meeting_rooms(v) << endl;
+
+    return 0;
+}
